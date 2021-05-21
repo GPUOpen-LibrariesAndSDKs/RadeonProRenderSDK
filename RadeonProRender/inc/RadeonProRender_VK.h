07931f59e7de9cf8d577a40f9f0779fbe36de548
RichardGe <richard.geslot@gmail.com>
2021-05-21 21:48:22 +0200

    update license - 2

--- RadeonProRender/inc/RadeonProRender_VK.h
+++ RadeonProRender/inc/RadeonProRender_VK.h
@@ -7,14 +7,6 @@
 *
 *  Copyright(C) 2011-2021 Advanced Micro Devices, Inc. All rights reserved.
 *
-*  All rights reserved.  This notice is intended as a precaution against
-*  inadvertent publication and does not imply publication or any waiver
-*  of confidentiality.  The year included in the foregoing notice is the
-*  year of creation of the work.
-*  @author Dmitry Kozlov (dmitry.kozlov@amd.com)
-*  @author Takahiro Harada (takahiro.harada@amd.com)
-*  @bug No known bugs.
-*
 \*****************************************************************************/
 #ifndef __RADEONPRORENDER_VK_H
 #define __RADEONPRORENDER_VK_H
--- tutorials/63_hybrid/main.cpp
+++ tutorials/63_hybrid/main.cpp
@@ -7,11 +7,6 @@
 *
 *  Copyright(C) 2011-2021 Advanced Micro Devices, Inc. All rights reserved.
 *
-*  All rights reserved.  This notice is intended as a precaution against
-*  inadvertent publication and does not imply publication or any waiver
-*  of confidentiality.  The year included in the foregoing notice is the
-*  year of creation of the work.
-*
 \*****************************************************************************/
 
 
